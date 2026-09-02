ede0bd727ab1ea44a5b94cca65b61204a137181d
clclement2 <clement.verin@etu.uca.fr>
2026-09-02 16:36:57 +0200

    Changement d'arborescence

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 607, hash: '1f6850c25b84862f4ed8137afb979a6b1cb0ce15b883e5d51c21542e0decba91', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1009, hash: '5f44b0ebb1c2671c9078e1c1af8f88f96c40e36b93b5f50d452a803ea1e8f4f2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 31826, hash: '1de08846361686614acc7bac8d94536ad24769d4023e120e1bc988f8917ef74e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 607, hash: '6263b15456248f8494707fc0df709220b9fb1b0b25f839141922ddcda22baa40', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1009, hash: '018545a8733165ac7a6a55b4492674ba15793b307e720aa03656b476cd8bab28', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 31676, hash: '4fa2d1efea5ff4e590a9de5dbab452bb8a85961e1a21eccd682dfd9b36e7a2d9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-VBYOOSCD.css': {size: 556, hash: 'wj91ISKNGlU', text: () => import('./assets-chunks/styles-VBYOOSCD_css.mjs').then(m => m.default)}
   },
 };
